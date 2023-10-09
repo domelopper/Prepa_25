46e8c2c97a71d976ff4bf344339202085424ca24
domelopper <dominikgalvan00@gmail.com>
2023-10-09 12:52:17 -0600

    letras ya jalan

--- Letras/letras.py
+++ Letras/letras.py
@@ -10,28 +10,29 @@ counter = 0
 
 
 while True:
-    ret,img = cap.read()
-    img = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2GRAY)
+    ret,frame = cap.read()
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     kernel = np.ones((1,1), np.uint8)
     img = cv2.dilate(img, kernel, iterations=1)
     img = cv2.erode(img, kernel, iterations=1)
-    img = threshold(cv2.medianBlur(img,3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    img = cv2.threshold(cv2.medianBlur(img,3), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
     if((counter%20)==0):
         imgH, imgW,_ = frame.shape
         x1,y1,w1,h1 = 0,0,imgH,imgW
         
-        imgchar = pytesseract.image_to_string(frame)
-        img_boxes = pytesseract.image_to_boxes(frame)
+        imgchar = pytesseract.image_to_string(img)
+        img_boxes = pytesseract.image_to_boxes(img)
+        print(imgchar)
 
         for boxes in img_boxes.splitlines():
             boxes = boxes.split(" ")
             x,y,w,h = int(boxes[1]), int(boxes[2]), int(boxes[3]), int(boxes[4])
             cv2.rectangle(frame,(x,imgH-y), (w,imgH-h), (0,0,255), 3)
 
-        cv2.putText(frame,imgchar, (x1 + int(w1/50), y1 + int(h1/50)), font, font_scale, (0,0,255))
+        cv2.putText(img,imgchar, (x1 + int(w1/50), y1 + int(h1/50)), font, font_scale, (0,0,255))
 
-    cv2.imshow("frame", frame)
+    cv2.imshow("frame", img)
     if cv2.waitKey(1) == ord("q"):
         break
 
